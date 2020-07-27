b951a13686ae0d57a53753e638f62eb4b42f6984
satellitex <s.a.t.e.3.ths@gmail.com>
2020-07-28 04:29:09 +0900

    added ovm and plasma types

---
+++ src/interfaces/ovm/definitions.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/camelcase */
+
+export default {
+    types: {
+        Decision: {
+            _enum: ["Undecided", "True", "False"]
+        },
+        ChallengeGameOf: {
+            propertyHash: "Hash",
+            challenges: "Vec<Hash>",
+            decision: "Decision",
+            createdBlock: "BlockNumber"
+        },
+        PredicateContractOf: {
+            predicateHash: "Hash",
+            inputs: "Vec<u8>"
+        },
+        PredicateHash: "Hash",
+        Schedule: {
+            version: "u32",
+            putCodePerByteCost: "Weight"
+        },
+        PrefabOvmModule: {
+            scheduleVersion: "u32",
+            code: "Vec<u8>"
+        },
+        Property: {
+            predicateAddress: "AccountId",
+            inputs: "Vec<Vec<u8>>"
+        },
+        PropertyOf: {
+            predicateAddress: "AccountId",
+            inputs: "Vec<Vec<u8>>"
+        },
+    }
+}
