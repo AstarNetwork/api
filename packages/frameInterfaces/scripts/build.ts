91118ed5f0996b070186b686939419eccbf2c891
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-05-13 00:22:00 +0900

    add shell execution command

--- packages/frameInterfaces/scripts/build.ts
+++ packages/frameInterfaces/scripts/build.ts
@@ -1,11 +1,48 @@
 #!/usr/bin/env ts-node
 
 import endPoints from './node-endpoints.json';
+import { exec } from 'child_process';
+
+const METADATA_REQ_PAYLOAD = {
+    id: '1',
+    jsonrpc: '2.0',
+    method: 'state_getMetadata',
+    params: [],
+};
+
+const DEFAULT_PORT = 9944;
+
+// a wrapper function that executes a shell command asynchronously
+const executeShell = async (cmd: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                console.warn(error);
+                reject(error);
+            }
+            resolve(stdout ? stdout : stderr);
+        });
+    });
+};
 
 const buildScript = async () => {
-    console.log('running the type generation script...');
+    const localNode = `http://localhost:${DEFAULT_PORT}`;
+
+    //! this requires curl and a running local node to work!
+    const result = await executeShell(
+        `curl -H "Content-Type: application/json" -d '${JSON.stringify(METADATA_REQ_PAYLOAD)}' ${localNode}`,
+    );
+
+    const nodeMetadata = JSON.parse(result).result;
+
+    console.log({ endpoint: localNode, metadata: nodeMetadata });
+
     endPoints.forEach((i) => {
-        console.log(i);
+        if (i.metadata.length > 0) {
+            console.log(`node ${i.network} has data`);
+        } else {
+            console.log(`node ${i.network} does not have any data`);
+        }
     });
 };
 
