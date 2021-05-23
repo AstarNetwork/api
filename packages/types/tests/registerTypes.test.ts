66efa7d5bd7c960f6c20c114e73a31e3c4cef835
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-05-23 16:29:42 +0900

    add basic test config (WIP)

---
+++ packages/types/tests/registerTypes.test.ts
@@ -0,0 +1,49 @@
+import { dustyDefinitions } from '../src';
+import { WsProvider, ApiPromise } from '@polkadot/api';
+import networkConfig from '../src/networkSpecs/network-config.json';
+
+const connectToNode = async (endpoint: string) => {
+    const wsProvider = new WsProvider(endpoint);
+    const api = await ApiPromise.create({
+        provider: wsProvider,
+    });
+
+    return await api.isReady;
+};
+
+describe('Establishing a connection with Plasm nodes', () => {
+    let api: ApiPromise;
+    const DUSTY_ENDPOINT = 'wss://rpc.dusty.plasmnet.io:9944';
+
+    // initialize the API connection before the test
+    beforeAll(async () => {
+        api = await connectToNode(DUSTY_ENDPOINT);
+    });
+
+    it('reads the chain modules', async () => {
+        const nodeModules = Object.keys(api.tx);
+
+        const dustyModules = networkConfig.dusty.customFrames;
+
+        console.log(nodeModules);
+
+        // register types on runtime
+        await api.registerTypes(dustyDefinitions);
+
+        // get the name of modules that are the same from the chain and the config file
+        const matchModules = dustyModules.filter((i) => {
+            const currentModule = nodeModules.find((modName) => {
+                return modName === i;
+            });
+            return currentModule;
+        });
+
+        console.log(matchModules);
+
+        expect(matchModules.length).toEqual(dustyModules.length);
+    });
+
+    afterAll(async () => {
+        await api.disconnect();
+    });
+});
