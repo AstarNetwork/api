64e1cbccb0031e7652b57f6c9042e91b51fbea7b
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-10-14 11:06:12 +0200

    update custom types and remove unused types

--- packages/types/src/networkSpecs/index.ts
+++ packages/types/src/networkSpecs/index.ts
@@ -1,3 +1,4 @@
 export { default as dustyDefinitions } from './dusty';
 export { default as plasmDefinitions } from './plasm';
-export { default as plasmCollatorDefinitions } from './plasmCollator';
+export { default as shidenDefinitions } from './shiden';
+export { default as shibuyaDefinitions } from './shibuya';
---
+++ packages/types/src/networkSpecs/shibuya.ts
@@ -0,0 +1,23 @@
+import type { RegistryTypes } from '@polkadot/types/types';
+
+const definitions: RegistryTypes = {
+    Keys: 'AccountId',
+    SmartContract: {
+        _enum: {
+            Evm: 'H160',
+            Wasm: 'AccountId',
+        },
+    },
+    EraStakingPoints: {
+        total: 'Balance',
+        stakers: 'BTreeMap<AccountId, Balance>',
+        formerStakedEra: 'EraIndex',
+        claimedRewards: 'Balance',
+    },
+    EraRewardAndStake: {
+        rewards: 'Balance',
+        staked: 'Balance',
+    },
+};
+
+export default definitions;
